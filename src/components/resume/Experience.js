f43f9ddd7df276e36c1f81a28bdb8b3958b51f55
RAkesh <rakeshchauhan8369262375@gmail.com>
2024-07-19 09:59:45 +0530

    experienceEdited

--- src/components/resume/Experience.js
+++ src/components/resume/Experience.js
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import { motion } from "framer-motion"
 import ResumeCard from "./ResumeCard";
 
@@ -7,16 +7,14 @@ const Experience = () => {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transition: { duration: 0.5 } }}
-      className="py-12 font-titleFont flex gap-20"
+      className="py-6 md:py-12 font-titleFont flex flex-col md:flex-row md:gap-20"
     >
-      <div>
-        <div className="flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">2023 - Current</p>
-          <h2 className="text-4xl font-bold">Job Experience</h2>
+      <div className="mb-10 md:mb-0">
+        <div className="flex flex-col gap-2 md:gap-4">
+          <p className="text-xs md:text-sm text-designColor tracking-[2px]">2023 - Current</p>
+          <h2 className="text-xl md:text-4xl font-bold">Job Experience</h2>
         </div>
-        <div className="mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
-
-
+        <div className="mt-8 md:mt-14 w-full md:w-auto md:min-w-[400px] max-w-full border-l-[6px] border-l-black border-opacity-30 flex flex-col md:gap-10">
           <ResumeCard
             title="Web Developer"
             subTitle="LeoGaurd - (2023 - 2024)"
@@ -26,19 +24,17 @@ const Experience = () => {
         </div>
       </div>
       <div>
-        <div className="flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">2001 - 2020</p>
-          <h2 className="text-4xl font-bold">Trainer Experience</h2>
+        <div className="flex flex-col gap-2 md:gap-4">
+          <p className="text-xs md:text-sm text-designColor tracking-[2px]">2001 - 2020</p>
+          <h2 className="text-xl md:text-4xl font-bold">Trainer Experience</h2>
         </div>
-        <div className="mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+        <div className="mt-8 md:mt-14 w-full md:w-auto md:min-w-[400px] max-w-full border-l-[6px] border-l-black border-opacity-30 flex flex-col md:gap-10">
           <ResumeCard
-            title=" Instructor"
+            title="Instructor"
             subTitle="career infotech (2022 - 2023)"
             result="Mumbai"
             des="Instructor at coding classes, guiding students in programming fundamentals and advanced techniques to foster technical skills and creativity."
           />
-
-
         </div>
       </div>
     </motion.div>
